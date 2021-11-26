fbc9e5e3743095de884481f18afa99910eaee59b
Anne-Sophie deguelle <adeguelle@sierrawireless.com>
2021-11-26 15:24:35 +0100

    remove .history

--- .history/src/ColorsUtils_20211124102648.ts
+++
@@ -1,7 +0,0 @@
-import { rgb, rgba } from "polished";
-
-export const Colors = {
-  black: "#000000",
-  blueSw: "#00677A",
-  tealSw: "#00AEAF",
-};
--- .history/src/components/Card/CustomCard.stories_20211114182603.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Meta } from "@storybook/react/types-6-0";
-import CustomCard from "./CustomCard";
-
-export default {
-  title: "Components/CustomCard",
-  component: CustomCard,
-} as Meta;
